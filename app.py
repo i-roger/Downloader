a8d6a1c2041088e7710f8c98587fc51e71039425
i-roger <mroger.dev@gmail.com>
2023-05-23 02:47:50 -0300

    Desenvolvendo app com python

---
+++ app.py
@@ -0,0 +1,35 @@
+import PySimpleGUI as sg
+from pytube import YouTube
+
+def window_init() :
+    sg.theme('DarkAmber')
+
+    layout = [
+        [sg.Text('Youtube Downloader', justification= 'center', size=(600))],
+        [sg.Input(key='link', justification= 'center', size=(600))],
+        [sg.Button('Download', expand_x=True), sg.Button('Alterar Diretorio', expand_x=True)]
+    ]
+
+    return sg.Window('Youtube Downloader Desenvolvido por: Matheus Roger', layout, size=(600,300))
+
+window = window_init()
+
+while True:
+    event, values = window.read()
+
+    if event == 'Download':
+        url = values['link']
+        yt = YouTube(str(url))
+        print("Title:", yt.title)
+        print("View:", yt.views)
+        yd = yt.streams.get_highest_resolution()
+        yd.download('D:\Downloads') # <----- Diretório! ##########################################
+        sg.popup('Download Completo!', 'Nome do Arquivo:', yt.title)
+
+    # ALTERAR LOCAL DE DOWNLOAD
+    #if event == 'Alterar Diretorio':
+
+
+    if event == sg.WIN_CLOSED:
+        window.close()
+        break
