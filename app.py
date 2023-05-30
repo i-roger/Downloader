78b09d3648f381731e1ea77f54e4ad3ad6e902c6
i-roger <mroger.dev@gmail.com>
2023-05-29 22:45:18 -0300

    AFix DDiretório de Downloads

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from pytube import YouTube
 import PySimpleGUI as sg
 
 def window_init() :
-    sg.theme('DarkAmber')
+    sg.theme('Black')
 
     layout = [
         [sg.Text('Video Downloader', justification= 'center', size=(600))],
         [sg.Input(key='link', justification= 'center', size=(600))],
-        [sg.Button('Download .MP4', expand_x=True), sg.Button('Download .MP3', expand_x=True), sg.Button('Alterar Diretorio', expand_x=True)]
+        [sg.Button('Download Video', expand_x=True), sg.Button('Download Audio', expand_x=True), sg.Button('Alterar Diretorio', expand_x=True)]
     ]
 
     return sg.Window('Video Downloader Desenvolvido por: Matheus Roger', layout, size=(600,300))
@@ -17,18 +17,18 @@ window = window_init()
 while True:
     event, values = window.read()
     #CODIGO FUNCIONANDO PORÉM FUNCIONA E DEPOIS PARA DE FUNCIONAR
-    if event == 'Download .MP4': #Trigger do botão DOWNLOAD MP4
+    if event == 'Download Video': #Trigger do botão DOWNLOAD MP4
         videolink = str(values['link'])
         if (videolink == '') :
             sg.popup('É necessário digitar a URL do video!!!', title='ERRO!')
         else :
             yt = YouTube(videolink)
             yd = yt.streams.get_highest_resolution()
-            destinoDownload = yd.download('D:/Downloads') # <----- Diretório! ##########################################
+            destinoDownload = yd.download('./Videos') # <----- Diretório! ##########################################
             sg.popup('Download Completo!', 'Nome do Arquivo:', yt.title, 'Visualizações:', yt.views, 'Arquivo em:', destinoDownload)
             print('Diretório do Download ========> ', destinoDownload)
 
-    if event == 'Download .MP3':
+    if event == 'Download Audio':
         videolink = str(values['link'])
         if (videolink == ''):
             sg.popup('É necessário digitar a URL do video!!!', title='ERRO!')
@@ -37,7 +37,7 @@ while True:
             yt = YouTube(videolink)
             yt.streams.filter(only_audio=True)
             stream = yt.streams.get_by_itag(140)
-            destinoDownload = stream.download(output_path='D:/Downloads')
+            destinoDownload = stream.download(output_path='./Audios')
             sg.popup('Download do Audio Completo!', 'Nome do Arquivo:', yt.title, 'Visualizações:', yt.views, 'Arquivo em:', destinoDownload)
             print('Diretório do Download ========> ', destinoDownload)
 
