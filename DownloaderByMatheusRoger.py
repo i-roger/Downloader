75ea93acc83c0db28357912f6aefffff6fe5b1ca
i-roger <mroger.dev@gmail.com>
2023-05-31 22:29:44 -0300

    Nova Atualização de interface!

--- DownloaderByMatheusRoger.py
+++ DownloaderByMatheusRoger.py
@@ -1,27 +1,81 @@
+from tkinter import messagebox
 import customtkinter as ctk
+from PIL import Image
 from pytube import YouTube
 
 window = ctk.CTk()
 window.geometry('800x500')
-window.title('App')
+window.title('Downloader | Desenvolvido por: Matheus Roger')
+window.iconbitmap("./icons/Downloader_icon.ico")
 window._set_appearance_mode('dark')
+window.resizable(width=False, height=False)
+
+### LOGO
+img = Image.open("./icons/logonew500x250.png")
+newImg = ctk.CTkImage(
+    dark_image=img,
+    size=(500,250)
+    )
+
+logoTopo = ctk.CTkLabel(window, image = newImg, text="")
+logoTopo.place(relx=0.5, rely=0.25, anchor=ctk.CENTER)
+### LOGO
+
+container = ctk.CTkFrame(window, width=650, height=150)
+container.place(relx=0.5, rely=0.70, anchor=ctk.CENTER)
+
+text1 = ctk.CTkLabel(container, text="Digite sua URL:")
+text1.place(relx=0.11, rely=0.15, anchor=ctk.CENTER)
+
+text2 = ctk.CTkLabel(container, text="Exemplo: (https://www.youtube.com/watch?v=xxxxxxxxxxxx)")
+text2.place(relx=0.7, rely=0.15, anchor=ctk.CENTER)
+
+textInput = ctk.CTkEntry(container, width=600)
+textInput.place(relx=0.5, rely=0.35, anchor=ctk.CENTER)
+
+btn1 = ctk.CTkButton(container,
+                     text="Download Video", 
+                     corner_radius=10,
+                     command= lambda: baixarVideo(),
+                     height=40
+                     )
+btn1.place(relx=0.30, rely=0.65, anchor=ctk.CENTER)
+
+btn2 = ctk.CTkButton(container, 
+                     text="Download Audio", 
+                     corner_radius=10,
+                     command= lambda: baixarAudio(),
+                     height=40
+                     )
+btn2.place(relx=0.70, rely=0.65, anchor=ctk.CENTER)
+
+def popupUrlVazia():
+    messagebox.showerror("Error !!!", "É necessário digitar a URL do video!")
+
 
 ### FUNÇÕES ###
 def baixarVideo():
     data = textInput.get()
 
     if (data == '') :
+        popupUrlVazia()
         print('É necessário digitar a URL do video!!!')
     else :
         yt = YouTube(data)
         yd = yt.streams.get_highest_resolution()
         dirDownload = yd.download('./Videos') # <----- Diretório! ##########################################
+
+        #downloadCompleto = str(yt.title, yt.views, dirDownload)
+        
         print('Download Completo!', 'Nome do Arquivo:', yt.title, 'Visualizações:', yt.views, 'Arquivo em:', dirDownload)
+
+        messagebox.showinfo("Download Completo!", message=downloadCompleto)
 
 def baixarAudio():
     data = textInput.get()
 
     if (data == '') :
+        popupUrlVazia()
         print('É necessário digitar a URL do video!!!')
     else :
         yt = YouTube(data)
@@ -30,37 +84,8 @@ def baixarAudio():
         dirDownload = stream.download(output_path='./Audios')
         print('Download do Audio Completo!', 'Nome do Arquivo:', yt.title, 'Visualizações:', yt.views, 'Arquivo em:', dirDownload)
 
-def sair():
+def sair(): ## Função para Fechar app...
     window.destroy()
 ### FUNÇÕES ###
-
-text = ctk.CTkLabel(window, text="Digite sua URL:")
-text.pack()
-
-textInput = ctk.CTkEntry(window, width=600)
-textInput.pack()
-
-btn1 = ctk.CTkButton(window, 
-                     text="Download Video", 
-                     corner_radius=10,
-                     command= lambda: baixarVideo()
-                     )
-btn1.pack(pady=20)
-
-btn2 = ctk.CTkButton(window, 
-                     text="Download Audio", 
-                     corner_radius=10, 
-                     command= lambda: baixarAudio()
-                     )
-btn2.pack(pady=20)
-
-btn3 = ctk.CTkButton(window, 
-                     text="Sair", 
-                     corner_radius=10, 
-                     fg_color='red', 
-                     hover_color='darkred',
-                     command= lambda: sair()
-                     )
-btn3.pack(pady=20)
 
 window.mainloop()
